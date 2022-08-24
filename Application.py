fa930926baeb02e0db03de0b02e85c07dfbecb5a
tree4553 <tree4553@naver.com>
2022-08-24 16:29:37 +0900

    호가 조회, 분봉 데이터 조회

---
+++ Application.py
@@ -0,0 +1,4 @@
+import module
+
+module.get_quote('BTC/USDT')
+module.before_data_min('BTC/USDT')
--- module.py
+++ module.py
@@ -4,11 +4,11 @@ import pandas as pd
 binance = ccxt.binance()
 btc = 'BTC/USDT'
 
-def show_ticker():
+def show_ticker(): # 티커 조회
     markets = binance.fetch_tickers()
     print(markets.keys())
 
-def current_price(name):
+def current_price(name): # 현재 가격 조회
     ticker = binance.fetch_ticker(name)
 
     print('%-10s' % 'Ticker: ', name)
@@ -18,23 +18,19 @@ def current_price(name):
     print('%-10s' % 'Low: ',    ticker['low'])
     print('%-10s' % 'Close: ',  ticker['close'])
     
-def current_price2(name):
+def current_price2(name): # 현재 가격 조회
     ticker = binance.fetch_ticker(name)
     pprint.pprint(ticker)
 
-def before_data(name):
+def before_data_min(name): # 1분봉 데이터 얻기
     ohlcvs = binance.fetch_ohlcv(name, timeframe='1m')
     df = pd.DataFrame(ohlcvs, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
     df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
     df.set_index('datetime', inplace=True)
     print(df)
 
-def get_quote(name):
+def get_quote(name): # 호가 조회
     orderbook = binance.fetch_order_book(name)
     pprint.pprint(orderbook['asks'])
     pprint.pprint(orderbook['bids'])
 
-#show_ticker()
-#current_price2(btc)
-#before_data(btc)
-get_quote(btc)
